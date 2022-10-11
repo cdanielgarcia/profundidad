9256703758bf20b55e30cdc5b13190603d8b9bea
Daniel Garcia <danielgarciaabello@gmail.com>
2022-10-10 23:03:34 -0500

    primer commit

---
+++ src/main/java/com/curso/springboot/profundidad/ProfundidadApplication.java
@@ -0,0 +1,13 @@
+package com.curso.springboot.profundidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfundidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfundidadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curso/springboot/profundidad/ProfundidadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.springboot.profundidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfundidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
